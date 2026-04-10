c31bcd71565026180e01273e31d14f710ccf7af8
Oktavianus Budi Santoso <oktavianusbudisantoso@gmail.com>
2026-04-10 15:51:45 +0700

    Lanjutan Belajar Laravel
    CRUD Sederhana, Seed sederhana, & Auth sederhana

---
+++ app/Http/Controllers/AdminController/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\AdminController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah',
+        ]);
+    }
+
+    public function showRegister()
+    {
+        return view('admin.auth.register');
+    }
+
+    public function register(Request $request)
+    {
+         return $request->all();
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}
---
+++ app/Http/Controllers/AdminController/TesterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\AdminController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TesterController extends Controller
+{
+    public function index(){
+        return "hello world";
+    }
+}
